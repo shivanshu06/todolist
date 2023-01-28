7b82e5e81010428987f1c0d8c01b3d23e50314f9
shivanshu06 <102288010+shivanshu06@users.noreply.github.com>
2023-01-29 00:19:21 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css"
+import Todolist from "./Todolist";
+
+
+
+function App() {
+  return (
+    <div>
+      
+      <Todolist />
+
+    </div>
+   
+  );
+};
+
+export default App;
---
+++ src/Todolist.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+function Todolist(){
+    const [activity,setactivity]=useState("");
+    const [listdata,setlistdata]=useState([]);
+    function addactivity(){
+        setlistdata((listdata)=>{
+            const updatedlist=[...listdata,activity]
+            console.log(updatedlist)
+            setactivity("")
+            return updatedlist
+        })
+    }
+
+    function removeactivity(i){
+        const updatedlistdata=listdata.filter((elements,id)=>{
+            return i!=id;
+        })
+        setlistdata(updatedlistdata);
+    }
+
+    function removeall(){
+        setlistdata('')
+    }
+
+    return(
+        <>
+        <div className="container">
+            <div className="header">Todoliiist</div>
+            <input type='text' placeholder="add activity" value={activity} onChange={(e)=>setactivity(e.target.value)}/>
+            <button onClick={addactivity}>add</button>
+            <p className='list-heading'>here is your list</p>
+            {listdata!=[] && listdata.map((data,i)=>{
+                return (
+                    <>
+                    <p key={i}>
+                        <div className="listdata">{data}
+                        <div> <button onClick={()=>removeactivity(i)}>remove</button></div>
+                        
+                        
+
+                        </div>
+                        
+                        </p> 
+                    </>
+                )
+            })}
+
+        </div>
+
+       {listdata.length>=1 &&  <button onClick={removeall}>remove all</button>}
+         </>
+         
+    )
+};
+
+export default Todolist
